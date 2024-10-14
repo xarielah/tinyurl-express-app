f21d884fc32a19dd3f052837868cb4f3eb1ae551
Ariel <arielah1997@gmail.com>
2024-10-14 23:47:10 +0300

    feat: add security for redirect

--- src/controllers/redirect/redirect.controller.ts
+++ src/controllers/redirect/redirect.controller.ts
@@ -6,6 +6,7 @@ import * as redirectService from "../../services/redirect/redirect.service";
 import { checkValidation } from "../../validators/check-validation";
 import { getRedirectInfoValidator } from "../../validators/redirect-validators/get-redirect-info.validator";
 import { RedirectPayload } from "./redirect.models";
+require("dotenv").config();
 const router = express.Router();
 
 router.post(
@@ -13,6 +14,12 @@ router.post(
   getRedirectInfoValidator,
   checkValidation,
   async (req: InternalRequest, res: Response) => {
+    const redirect_key = req.headers ? req.headers["x-redirect-key"] : "";
+    const secret_redirect_key = process.env.SECRET_REDIRECT_KEY || "";
+    if (!redirect_key || redirect_key !== secret_redirect_key)
+      return res
+        .status(400)
+        .send({ message: RedirectErrors.REDIRECT_KEY_NOT_FOUND });
     const body = req.body as RedirectPayload;
     const locationInformation = body.locationInformation;
     const shortId = body.shortId || "";
--- src/lib/error-handling/redirect-error.type.ts
+++ src/lib/error-handling/redirect-error.type.ts
@@ -2,4 +2,5 @@ export enum RedirectErrors {
   REDIRECT_CODE_NOT_FOUND = "Redirect code or short id did not match any records",
   REDIRECT_CODE_INVALID = "Redirect code must exist in request and be between 10-11 characters long",
   REDIRECT_LOCATION_INFORMATION_INVALID = "Location information must include country, country region, latitude, longitude, and flag",
+  REDIRECT_KEY_NOT_FOUND = "You cannot access this resource this way",
 }
